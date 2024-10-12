92692ea8ed7fb8cec86c7fcae545361c61682312
TranQuoc <tranquoc1301@gmail.com>
2024-10-12 15:12:07 +0700

    fix validator

--- assets/js/validator.js
+++ assets/js/validator.js
@@ -1,215 +1,168 @@
 function Validator(formSelector) {
-    var _this = this;
-    var formRules = {};
+  var _this = this;
 
-    function getParent(element, selector) {
-        while (element.parentElement) {
-            if (element.parentElement.matches(selector)) {
-                return element.parentElement;
-            }
-            element = element.parentElement;
-        }
-    }
+  var formElement = document.querySelector(formSelector);
 
-    var validatorRules = {
-        required: function (value) {
-            return value ? undefined : 'Vui lòng nhập trường này !';
-        },
-        email: function (value) {
-            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            return regex.test(value) ? undefined : 'Vui lòng nhập email hợp lệ !';
-        },
-        min: function (min) {
-            return function (value) {
-                return value.length >= min ? undefined : `Vui lòng nhập ít nhất ${min} kí tự !`;
-            }
-        },
-        max: function (max) {
-            return function (value) {
-                return value.length <= max ? undefined : `Tối đa ${max} kí tự !`;
-            }
-        },
-        isConfirmed: function (getConfirmValue, message) {
-            return function (value) {
-                return value === getConfirmValue() ? undefined : message || 'Giá trị nhập vào không chính xác !';
-            }
-        },
-        isGenderSelected: function () {
-            return function () {
-                var genderInputs = document.querySelectorAll('input[name="gender"]');
-                for (var genderInput of genderInputs) {
-                    if (genderInput.checked) {
-                        return undefined;
-                    }
-                }
-                return 'Vui lòng chọn giới tính !';
-            }
-        }
-    };
+  var formRules = {};
 
-    // Lấy ra formElement trong DOM
-    var formElement = document.querySelector(formSelector);
+  // Định nghĩa các rules
+  var validatorRules = {
+    required: function (value) {
+      return value ? undefined : "Vui lòng nhập trường này";
+    },
+    email: function (value) {
+      var regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+      return regex.test(value) ? undefined : "Vui lòng nhập email hợp lệ";
+    },
+    min: function (min) {
+      return function (value) {
+        return value.length >= min
+          ? undefined
+          : `Vui lòng nhập ít nhất ${min} ký tự`;
+      };
+    },
+    max: function (max) {
+      return function (value) {
+        return value.length <= max
+          ? undefined
+          : `Vui lòng nhập không quá ${max} ký tự`;
+      };
+    },
+    isConfirmed: function (value, getConfirmValue) {
+      return value === getConfirmValue()
+        ? undefined
+        : "Giá trị nhập lại không chính xác";
+    },
+  };
 
-    if (formElement) {
-        var inputs = formElement.querySelectorAll('[name][rules]');
-        for (var input of inputs) {
-            var rules = input.getAttribute('rules').split('|');
-            for (var rule of rules) {
-                var ruleInfo;
-                var isRuleHasValue = rule.includes(':');
-                if (isRuleHasValue) {
-                    ruleInfo = rule.split(':');
-                    rule = ruleInfo[0];
-                }
-                var ruleFunc = validatorRules[rule];
+  // Lấy ra các input từ form
+  if (formElement) {
+    var inputs = formElement.querySelectorAll("[name][rules]");
 
-                if (isRuleHasValue) {
-                    ruleFunc = ruleFunc(ruleInfo[1]);
-                }
+    for (var input of inputs) {
+      var rules = input.getAttribute("rules").split("|");
 
-                if (Array.isArray(formRules[input.name])) {
-                    formRules[input.name].push(ruleFunc);
-                } else {
-                    formRules[input.name] = [ruleFunc];
-                }
-            }
-            //Event Listener để validate (blur, change, ...)
-            input.onblur = handleValidate;
-            input.oninput = handleClearError;
+      for (var rule of rules) {
+        var ruleInfo;
+        var isRuleHasValue = rule.includes(":");
+
+        if (isRuleHasValue) {
+          ruleInfo = rule.split(":");
+          rule = ruleInfo[0];
         }
 
-        // Add gender validation manually since it uses radio buttons
-        var genderInputs = formElement.querySelectorAll('input[name="gender"]');
-        if (genderInputs.length) {
-            for (var genderInput of genderInputs) {
-                genderInput.onchange = handleValidate;
-            }
-            formRules['gender'] = [validatorRules['isGenderSelected']()];
+        var ruleFunc = validatorRules[rule];
+
+        if (isRuleHasValue) {
+          ruleFunc = ruleFunc(ruleInfo[1]);
         }
 
-        // Add password confirmation validation manually
-        var passwordConfirmationInput = formElement.querySelector('input[name="password_confirmation"]');
-        if (passwordConfirmationInput) {
-            passwordConfirmationInput.onblur = handleValidate;
-            passwordConfirmationInput.oninput = handleClearError;
-            formRules['password_confirmation'].push(validatorRules['isConfirmed'](
-                function () {
-                    return formElement.querySelector('input[name="password"]').value;
-                },
-                'Mật khẩu nhập lại không đúng !'
-            ));
-        }
-
-        //Hàm thực hiện validate
-        function handleValidate(event) {
-            var rules = formRules[event.target.name];
-            var errorMessage;
-
-            rules.some(function (rule) {
-                errorMessage = rule(event.target.value);
-                return errorMessage;
-            });
-
-            if (errorMessage) {
-                var formGroup = getParent(event.target, '.form-group');
-                if (formGroup) {
-                    formGroup.classList.add('invalid');
-
-                    var formMessage = formGroup.querySelector('.form-message');
-                    if (formMessage) {
-                        formMessage.innerText = errorMessage;
-                    }
-                }
-            } else {
-                var formGroup = getParent(event.target, '.form-group');
-                if (formGroup) {
-                    formGroup.classList.remove('invalid');
-                    var formMessage = formGroup.querySelector('.form-message');
-                    if (formMessage) {
-                        formMessage.innerText = '';
-                    }
-                }
-            }
-
-            return !errorMessage;
-        }
-
-        // Hàm clear error message khi nhập vào input
-        function handleClearError(event) {
-            var formGroup = getParent(event.target, '.form-group');
-            if (formGroup.classList.contains('invalid')) {
-                formGroup.classList.remove('invalid');
-
-                var formMessage = formGroup.querySelector('.form-message');
-                if (formMessage) {
-                    formMessage.innerText = '';
-                }
-            }
-        }
-    }
-
-    // Xử lí hành vi submit form
-    formElement.onsubmit = function (event) {
-        event.preventDefault();
-
-        var inputs = formElement.querySelectorAll('[name][rules]');
-        var isValid = true;
-
-        for (var input of inputs) {
-            if (!handleValidate({ target: input })) {
-                isValid = false;
-            }
-        }
-
-        var genderErrorMessage = validatorRules.isGenderSelected()();
-        var genderFormGroup = document.querySelector('.gender-options').closest('.form-group');
-        if (genderErrorMessage) {
-            isValid = false;
-            genderFormGroup.classList.add('invalid');
-            genderFormGroup.querySelector('.form-message').innerText = genderErrorMessage;
+        if (Array.isArray(formRules[input.name])) {
+          formRules[input.name].push(ruleFunc);
         } else {
-            genderFormGroup.classList.remove('invalid');
-            genderFormGroup.querySelector('.form-message').innerText = '';
+          formRules[input.name] = [ruleFunc];
         }
+      }
 
-        if (isValid) {
-            if (typeof _this.onSubmit === 'function') {
-                var enableInputs = formElement.querySelectorAll('[name]');
-                var formValues = Array.from(enableInputs).reduce(function (values, input) {
-
-                    switch (input.type) {
-                        case 'radio':
-                            if (input.checked) {
-                                values[input.name] = input.value;
-                            }
-                            break;
-                        case 'checkbox':
-                            if (!input.matches(':checked')) {
-                                values[input.name] = '';
-                                return values;
-                            }
-
-                            if (!Array.isArray(values[input.name])) {
-                                values[input.name] = [];
-                            }
-
-                            values[input.name].push(input.value);
-                            break;
-                        case 'file':
-                            values[input.name] = input.files;
-                            break;
-                        default:
-                            values[input.name] = input.value;
-                            break;
-                    }
-
-                    return values;
-                }, {});
-
-                _this.onSubmit(formValues);
-            } else {
-                formElement.submit();
-            }
-        }
+      // Lắng nghe sự kiện để validate
+      input.onblur = handleValidate;
+      input.oninput = handleClearError;
     }
+
+    // Hàm thực hiện validate
+    function handleValidate(event) {
+      var rules = formRules[event.target.name];
+      var errorMessage;
+
+      for (var rule of rules) {
+        switch (event.target.name) {
+          case "password_confirmation":
+            errorMessage = rule(event.target.value, function () {
+              return formElement.querySelector("#password").value;
+            });
+            break;
+          default:
+            errorMessage = rule(event.target.value);
+        }
+
+        if (errorMessage) break;
+      }
+
+      // Hiển thị lỗi ra UI
+      var formGroup = event.target.closest(".form-group");
+      var formMessage = formGroup.querySelector(".form-message");
+
+      if (errorMessage) {
+        formGroup.classList.add("invalid");
+        formMessage.innerText = errorMessage;
+      } else {
+        formGroup.classList.remove("invalid");
+        formMessage.innerText = "";
+      }
+
+      return !errorMessage;
+    }
+
+    // Hàm clear error message
+    function handleClearError(event) {
+      var formGroup = event.target.closest(".form-group");
+      var formMessage = formGroup.querySelector(".form-message");
+
+      if (formGroup.classList.contains("invalid")) {
+        formGroup.classList.remove("invalid");
+        formMessage.innerText = "";
+      }
+    }
+
+    // Xử lý hành động submit form
+    formElement.onsubmit = function (event) {
+      event.preventDefault();
+
+      var isValid = true;
+
+      for (var input of inputs) {
+        if (!handleValidate({ target: input })) {
+          isValid = false;
+        }
+      }
+
+      if (isValid) {
+        if (typeof _this.onSubmit === "function") {
+          var enableInputs = formElement.querySelectorAll("[name]");
+          var formValues = Array.from(enableInputs).reduce(function (
+            values,
+            input
+          ) {
+            switch (input.type) {
+              case "radio":
+                values[input.name] = formElement.querySelector(
+                  'input[name="' + input.name + '"]:checked'
+                ).value;
+                break;
+              case "checkbox":
+                if (!input.matches(":checked")) {
+                  values[input.name] = "";
+                  return values;
+                }
+                if (!Array.isArray(values[input.name])) {
+                  values[input.name] = [];
+                }
+                values[input.name].push(input.value);
+                break;
+              case "file":
+                values[input.name] = input.files;
+                break;
+              default:
+                values[input.name] = input.value;
+            }
+
+            return values;
+          },
+          {});
+
+          _this.onSubmit(formValues);
+        }
+      }
+    };
+  }
 }
